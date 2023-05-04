b94503f95170940757af186a681cf0a37ac99b57
Abhinav Nath <abhinavnath@ymail.com>
2023-05-04 23:49:28 +0530

    Initial setup

---
+++ src/main/java/com/codecafe/springbootkit/SpringbootStarterKitApplication.java
@@ -0,0 +1,13 @@
+package com.codecafe.springbootkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStarterKitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStarterKitApplication.class, args);
+	}
+
+}
