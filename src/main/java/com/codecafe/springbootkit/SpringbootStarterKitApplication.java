2788cf607b943a027bcdbc14c205a62245377f6d
Abhinav Nath <abhinavnath@ymail.com>
2023-05-04 23:54:31 +0530

    Add .editorconfig

--- src/main/java/com/codecafe/springbootkit/SpringbootStarterKitApplication.java
+++ src/main/java/com/codecafe/springbootkit/SpringbootStarterKitApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootStarterKitApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootStarterKitApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootStarterKitApplication.class, args);
+  }
 
 }
